fa1cc5cbc36cb0e671ef8e7af1bf9e658c39c754
Yik Wai Chan <yichan@u91z06s03.42kl.edu.my>
2023-03-22 02:54:33 +0800

    add

--- ex01/srcs/Contact.cpp
+++ ex01/srcs/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: yichan <yichan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 00:43:13 by yichan            #+#    #+#             */
-/*   Updated: 2023/03/21 20:08:44 by yichan           ###   ########.fr       */
+/*   Updated: 2023/03/21 22:44:13 by yichan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ Contact::~Contact(void)
 std::string Contact::inputData(const char *prompt)
 {
 	std::string value;
-
+	
 	while(value.empty())
 	{
 		std::cout << prompt;
@@ -33,6 +33,12 @@ std::string Contact::inputData(const char *prompt)
 		else if (value.find_first_not_of(" \t\v\r\n") == value.npos)
 			value.clear();
 	}
+	// while (value.find("\t") != std::string::npos)
+	// 	value = value.replace(value.find("\t"), std::strlen("\t"), "    ");
+	for (size_t i = 0; i < value.size(); i++) {
+		if (value[i] == '\t')
+			value.replace(i, std::strlen("\t"), "    ");
+}
 	return value;
 }
 
@@ -63,10 +69,10 @@ std::string	Contact::getDarkSecret(void) const
 
 void	Contact::setFname(void)
 {
-	std::string	str = inputData("Enter a First Name: ");
-	while (str.find("\t") != std::string::npos)
-		str = str.replace(str.find("\t"), std::strlen("\t"), "    ");
-	this->first_name = str;
+	// std::string	str = inputData("Enter a First Name: ");
+	// while (str.find("\t") != std::string::npos)
+	// 	str = str.replace(str.find("\t"), std::strlen("\t"), " ");
+	this->first_name = inputData("Enter a First Name: ");
 }
 
 void	Contact::setLname(void)
